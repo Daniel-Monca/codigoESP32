62a7bced62b3013b9154a3305cf91c8498c3b234
Daniel-Monca <87156710+Daniel-Monca@users.noreply.github.com>
2023-05-29 00:07:47 -0500

    Add files via upload

---
+++ CodigoDaniel.ino
@@ -0,0 +1,55 @@
+//Paso 1
+#include <WiFi.h>
+#include <ESP32Servo.h> 
+
+#include <FirebaseESP32.h>
+
+const char* ssid = "ROCHI";
+const char* password = "17268681";
+
+const char* firebaseHost = "https://cerraduraelectronica-fae61-default-rtdb.firebaseio.com/";
+const char* firebaseAuth = "WurEOHP01KKH4XHJTDhl6EBooBQuM3t7sBrIepfZ";
+
+Servo myservo; 
+int pos = 0;
+
+FirebaseData firebaseData;
+
+void setup() {
+  myservo.attach(13);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Conectando a Wi-Fi...");
+  }
+  Serial.println("Conectado a Wi-Fi.");
+  
+  Firebase.begin(firebaseHost, firebaseAuth);
+}
+
+void loop() {
+  if (Firebase.get(firebaseData, "/ESP32")) {
+    if (firebaseData.dataType() == "string") {
+      String value = firebaseData.stringData();
+      Serial.print("Valor obtenido: ");
+      Serial.println(value);
+      if(value == "1"){
+        for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+          // in steps of 1 degree
+          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+          delay(15);                       // waits 15ms for the servo to reach the position
+        }
+        delay(61000);
+
+        for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+          delay(15);                       // waits 15ms for the servo to reach the position
+        }
+      }
+      
+    }
+  }
+ 
+  delay(1000);
+}
